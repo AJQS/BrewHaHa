3322bbe77b8d3995ddbdef1309d16c24073193dd
Austin Quattrocchi-Smuda <AustinJSmuda@icloud.com>
2019-02-04 22:37:43 -0700

    tester

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,6 +18,7 @@ import Strapi from "strapi-sdk-javascript/build/main";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
+//stupid save
 class App extends Component {
   state = {
     brands: [],
